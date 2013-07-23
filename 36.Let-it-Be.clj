f110da6635cb206672b292e3eccca9ff7bafdccd
Yamamoto Yusuke <y.yamamoto88@gmail.com>
2013-07-23 23:16:03 +0900

    add 36.Let it Be

---
+++ 36.Let-it-Be.clj
@@ -0,0 +1,12 @@
+;Let it Be
+;Difficulty:	Elementary
+;Topics:	math syntax
+;
+;
+;Can you bind x, y, and z so that these are all true?
+;
+;(= 10 (let __ (+ x y)))
+;(= 4 (let __ (+ y z)))
+;(= 1 (let __ z))
+
+[x 7 y 3 z 1]
